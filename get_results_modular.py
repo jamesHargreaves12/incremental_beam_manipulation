45308800d2d7f22a178b3e42bbc8189aa3e18bbb
james_hargreaves <jh2045@cam.ac.uk>
2020-05-04 02:28:33 +0100

    ignore

--- get_results_modular.py
+++ get_results_modular.py
@@ -63,7 +63,7 @@ for beam_size in cfg["beam_sizes"]:
                                           greedy_complete=greedy_complete,
                                           max_pred_len=60,
                                           save_progress_path=cfg.get('save_progress_file', None),
-                                          also_rerank_final=cfg.get('also_rerank_final', False)
+                                          also_rerank_final=cfg.get('also_rerank_final', False),
                                           cfg=cfg)
 
     preds = [[x for x in pred if x not in [START_TOK, END_TOK, PAD_TOK]] for pred in preds]
