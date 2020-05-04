2c07c1ce5daa220d63dde44f63b09fee0019c162
james_hargreaves <jh2045@cam.ac.uk>
2020-05-04 20:55:34 +0100

    ignore

--- reimplement_reinforce.py
+++ reimplement_reinforce.py
@@ -95,9 +95,9 @@ def order_beam_acording_to_rescorer(rescorer, beam, da_emb, i, cfg, out_beam=Non
 
     if sections_flag:
         num_ranks = cfg["train_reranker"]["num_ranks"]
-        if cfg["train_reranker"]["with_regs_train"]:
-            num_ranks += 1
         cut_offs = get_section_cutoffs(num_ranks, cfg["merge_middle_sections"])
+        if cfg["train_reranker"]["with_regs_train"]:
+            cut_offs = [0.2] + [x*0.8+0.2 for x in cut_offs]
         scored_finished_beams = score_beams(rescorer, beam, da_emb, i)
         sections = [get_section_value(cut_offs, x) for (x, _), _ in scored_finished_beams]
         # sections = relative_to_sections([s for (s, t), _ in scored_finished_beams])
